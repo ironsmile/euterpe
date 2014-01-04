00b38804a9ac0fd258d0b5bbf6f6f170e0514423
Doychin Atanasov <ironsmile@gmail.com>
2014-01-04 12:09:19 +0200

    Testing the Go HTTP and HTML libraries

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ironsmile/httpms/src"
+
+func main() {
+	src.Main()
+}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,63 @@
-package main
+package src
 
+import "net/http"
+import "log"
+import "time"
 import "fmt"
+import "os"
+import "html/template"
 
-func main() {
-	fmt.Println("Hello World")
+type SearchHandler struct{}
+
+func (sh SearchHandler) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+
+	fullPath := fmt.Sprintf("%s?%s", req.URL.Path, req.URL.RawQuery)
+
+	templateDir := fmt.Sprintf("%s/src/github.com/ironsmile/httpms/templates",
+		os.ExpandEnv("$GOPATH"))
+	templateFile := fmt.Sprintf("%s/test2.html", templateDir)
+
+	_, err := os.Stat(templateFile)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte(fmt.Sprintf("%s does not exist", templateFile)))
+		return
+	}
+
+	tmpl, err := template.ParseFiles(templateFile)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte(fmt.Sprintf("Error parsing template %s", templateFile)))
+		return
+	}
+
+	variables := map[string]string{
+		"FullPath":     fullPath,
+		"TemplateFile": templateFile,
+	}
+
+	err = tmpl.Execute(writer, variables)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte("Error executing template"))
+		return
+	}
+}
+
+func Main() {
+
+	http.Handle("/", http.FileServer(http.Dir("/home/iron4o/workspace")))
+	http.Handle("/search/", SearchHandler{})
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
