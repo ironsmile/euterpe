c62d61349e6a83a81ea031248e83db83f9e0b1e1
Doychin Atanasov <ironsmile@gmail.com>
2014-01-26 03:46:46 +0200

    Functionality for merging the user config over the default one and tests for it.

--- src/config/config.go
+++ src/config/config.go
@@ -38,13 +38,20 @@ type Config struct {
 	HTTPRoot       string     `json:"http_root"`
 }
 
+// Used for merging one config over the other. I need the zero value for every
+// Field to be nil so that I can destinguish if it has been in the json file
+// or not. If I did not use pointers I would not have been able to do that.
+// That way the merged (user) json can contain a subset of all fields and everything
+// else will be used from the default json.
+// Unfortunatelly this leads to repetition since MergedConfig must have the same
+// fields in the same order as Config.
 type MergedConfig struct {
 	Listen         *string     `json:"listen"`
 	SSL            *bool       `json:"ssl"`
 	SSLCertificate *ConfigCert `json:"ssl_certificate"`
 	Auth           *bool       `json:"basic_authenticate"`
 	Authenticate   *ConfigAuth `json:"authentication"`
-	Libraries      []string    `json:"libraries"`
+	Libraries      *[]string   `json:"libraries"`
 	UserPath       *string     `json:"user_path"`
 	LogFile        *string     `json:"log_file"`
 	SqliteDatabase *string     `json:"sqlite_database"`
@@ -81,54 +88,62 @@ func (cfg *Config) FindAndParse() error {
 		return err
 	}
 
-	usrCfg := new(Config)
+	defaultConfig, err := ioutil.ReadFile(cfg.UserConfigPath())
 
-	err = usrCfg.parse(cfg.UserConfigPath())
+	if err != nil {
+		return err
+	}
+
+	return cfg.mergeJSON(defaultConfig)
+}
+
+// The config object parses an json file and populates its fields.
+// The json file is specified by the finame argument.
+func (cfg *Config) parse(filename string) error {
+	jsonContents, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(jsonContents, cfg)
+}
+
+// Parses the json buffer jsonBuffer into a MergedConfig and uses it
+// for cfg.merge
+func (cfg *Config) mergeJSON(jsonBuffer []byte) error {
+
+	usrCfg := new(MergedConfig)
+
+	err := json.Unmarshal(jsonBuffer, usrCfg)
 
 	if err != nil {
 		return err
 	}
 
 	cfg.merge(usrCfg)
-
 	return nil
 }
 
-// The config object parses an json file and populates its fields.
-// The json file is specified by the finame argument.
-func (cfg *Config) parse(filename string) error {
-	defaultConfig, err := ioutil.ReadFile(filename)
-
-	if err != nil {
-		return err
-	}
-
-	return json.Unmarshal(defaultConfig, cfg)
-}
-
-// Merges an other config on top of itself. Only non-zero values will be merged.
-func (cfg *Config) merge(merged *Config) {
+// Merges an MergedConfig on top of itself. Only non-zero values will be merged.
+func (cfg *Config) merge(merged *MergedConfig) {
 	cfgVal := reflect.ValueOf(cfg).Elem()
 	mergedVal := reflect.ValueOf(merged).Elem()
 
 	for i := 0; i < mergedVal.NumField(); i++ {
 		mergedField := mergedVal.Field(i)
-		if !mergedField.IsValid() {
+		if !mergedField.IsValid() || mergedField.IsNil() {
 			continue
 		}
-
-		if reflect.Zero(mergedField.Type()) == mergedField {
-			continue
-		}
-
 		cfgField := cfgVal.Field(i)
-
 		if !cfgField.CanSet() {
 			continue
 		}
-
-		//log.Printf("Merging %#v\n", mergedField)
-		cfgField.Set(mergedField)
+		if mergedField.Kind() != reflect.Ptr {
+			cfgField.Set(mergedField)
+			continue
+		}
+		cfgField.Set(reflect.Indirect(mergedField))
 	}
 }
 
--- src/config/config_test.go
+++ src/config/config_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -30,7 +31,7 @@ func TestFindTheRighConfigFile(t *testing.T) {
 
 func TestMergingConfigs(t *testing.T) {
 	cfg := new(Config)
-	merged := new(Config)
+	merged := new(MergedConfig)
 
 	cfg.SSL = true
 
@@ -40,7 +41,8 @@ func TestMergingConfigs(t *testing.T) {
 		t.Errorf("Zero value from the merged has been copied over")
 	}
 
-	merged.Listen = ":http"
+	str := ":http"
+	merged = &MergedConfig{Listen: &str}
 
 	cfg.merge(merged)
 
@@ -48,70 +50,128 @@ func TestMergingConfigs(t *testing.T) {
 		t.Errorf("NonZero value has not been copied over")
 	}
 
-	cfg.SSL = false
-	cfg.Listen = ":80"
-	cfg.LogFile = "logfile"
-	cfg.Gzip = true
-	cfg.ReadTimeout = 10
-	cfg.WriteTimeout = 10
-	cfg.MaxHeadersSize = 100
-	cfg.SqliteDatabase = "httpms.db"
-	cfg.Auth = true
-	cfg.Authenticate = ConfigAuth{User: "bob", Password: "marley"}
+	getDefaultCfg := func() *Config {
+		dflConfig := new(Config)
+		dflConfig.SSL = false
+		dflConfig.Listen = ":80"
+		dflConfig.LogFile = "logfile"
+		dflConfig.Gzip = true
+		dflConfig.ReadTimeout = 10
+		dflConfig.WriteTimeout = 10
+		dflConfig.MaxHeadersSize = 100
+		dflConfig.SqliteDatabase = "httpms.db"
+		dflConfig.Auth = true
+		dflConfig.Authenticate = ConfigAuth{User: "bob", Password: "marley"}
+		return dflConfig
+	}
 
-	merged.Listen = ":8080"
-	merged.SSL = true
-	merged.SSLCertificate = ConfigCert{Crt: "crt", Key: "key"}
-	merged.Libraries = append(merged.Libraries, "/some/path")
-	merged.Auth = false
+	cfg = getDefaultCfg()
+
+	merged.Listen = new(string)
+	*merged.Listen = ":8080"
+	merged.SSL = new(bool)
+	*merged.SSL = true
+	merged.SSLCertificate = new(ConfigCert)
+	*merged.SSLCertificate = ConfigCert{Crt: "crt", Key: "key"}
+	merged.Libraries = new([]string)
+	*merged.Libraries = append(*merged.Libraries, "/some/path")
+	merged.Auth = new(bool)
+	*merged.Auth = false
 
 	cfg.merge(merged)
 
-	if cfg.SSL != true {
-		t.Errorf("SSL was false but it was expected to be true")
+	checkMerge := func(cfg *Config) {
+
+		if cfg.SSL != true {
+			t.Errorf("SSL was false but it was expected to be true")
+		}
+
+		if cfg.Auth != false {
+			t.Errorf("Auth was true but it was expected to be false")
+		}
+
+		if cfg.SSLCertificate.Crt != "crt" || cfg.SSLCertificate.Key != "key" {
+			t.Errorf("SSL Certificate was not as expected: %#v", cfg.SSLCertificate)
+		}
+
+		if cfg.Authenticate.User != "bob" || cfg.Authenticate.Password != "marley" {
+			t.Errorf("Authenticate user and password were wrong: %#v", cfg.Authenticate)
+		}
+
+		if cfg.Listen != ":8080" {
+			t.Errorf("Listen was %s", cfg.Listen)
+		}
+
+		if cfg.Gzip != true {
+			t.Errorf("Gzip was %t", cfg.Gzip)
+		}
+
+		if cfg.ReadTimeout != 10 {
+			t.Errorf("ReadTimeout was %d", cfg.ReadTimeout)
+		}
+
+		if cfg.WriteTimeout != 10 {
+			t.Errorf("WriteTimeout was %d", cfg.WriteTimeout)
+		}
+
+		if cfg.MaxHeadersSize != 100 {
+			t.Errorf("MaxHeadersSize was %d", cfg.MaxHeadersSize)
+		}
+
+		if cfg.SqliteDatabase != "httpms.db" {
+			t.Errorf("SqliteDatabase was %s", cfg.SqliteDatabase)
+		}
+
+		if len(cfg.Libraries) != 1 {
+			t.Errorf("Libraries was not as expected: %#v", cfg.Libraries)
+		} else {
+			if cfg.Libraries[0] != "/some/path" {
+				t.Errorf("Library was wrong: %s", cfg.Libraries[0])
+			}
+		}
 	}
 
-	if cfg.Auth != false {
-		t.Errorf("Auth was true but it was expected to be false")
+	checkMerge(cfg)
+
+	cfg = getDefaultCfg()
+	cfg.mergeJSON([]byte(`
+		{
+			"listen": ":8080",
+			"ssl": true,
+			"ssl_certificate": {
+				"key": "key",
+				"crt": "crt"
+			},
+			"libraries": ["/some/path"],
+			"basic_authenticate": false
+		}
+	`))
+	checkMerge(cfg)
+}
+
+func TestMergedConfigHasTheSameFieldsAsConfig(t *testing.T) {
+	configType := reflect.TypeOf(Config{})
+	mergedType := reflect.TypeOf(MergedConfig{})
+
+	if configType.NumField() != mergedType.NumField() {
+		t.Fatalf("Different number of fields in Config and MergedConfig")
 	}
 
-	if cfg.SSLCertificate.Crt != "crt" || cfg.SSLCertificate.Key != "key" {
-		t.Errorf("SSL Certificate was not as expected: %#v", cfg.SSLCertificate)
-	}
+	for i := 0; i < configType.NumField(); i++ {
+		configField := configType.Field(i)
+		mergedField := mergedType.Field(i)
 
-	if cfg.Authenticate.User != "bob" || cfg.Authenticate.Password != "marley" {
-		t.Errorf("Authenticate user and password were wrong: %#v", cfg.Authenticate)
-	}
+		if configField.Name != mergedField.Name {
+			t.Errorf("Different field names: %s and %s", configField.Name,
+				mergedField.Name)
+		}
 
-	if cfg.Listen != ":8080" {
-		t.Errorf("Listen was %s", cfg.Listen)
-	}
+		if mergedField.Type.Kind() != reflect.Ptr {
+			t.Errorf("MergedConfig field %s was not a pointer", mergedField.Name)
+		}
 
-	if cfg.Gzip != true {
-		t.Errorf("Gzip was %t", cfg.Gzip)
-	}
-
-	if cfg.ReadTimeout != 10 {
-		t.Errorf("ReadTimeout was %d", cfg.ReadTimeout)
-	}
-
-	if cfg.WriteTimeout != 10 {
-		t.Errorf("WriteTimeout was %d", cfg.WriteTimeout)
-	}
-
-	if cfg.MaxHeadersSize != 100 {
-		t.Errorf("MaxHeadersSize was %d", cfg.MaxHeadersSize)
-	}
-
-	if cfg.SqliteDatabase != "httpms.db" {
-		t.Errorf("SqliteDatabase was %s", cfg.SqliteDatabase)
-	}
-
-	if len(cfg.Libraries) != 1 {
-		t.Errorf("Libraries was not as expected: %#v", cfg.Libraries)
-	} else {
-		if cfg.Libraries[0] != "/some/path" {
-			t.Errorf("Library was wrong: %s", cfg.Libraries[0])
+		if configField.Tag != mergedField.Tag {
+			t.Errorf("MergedConfig struct tag for %s was different", mergedField.Name)
 		}
 	}
 }
