36644329764e89d7bcf8d42ad707f2eb1b936d6f
Doychin Atanasov <ironsmile@gmail.com>
2015-12-05 12:27:34 +0200

    Local library source files split

---
+++ src/library/local_library_scan.go
@@ -0,0 +1,87 @@
+package library
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+//!TODO: make scan also remove files which have been deleted since the previous scan
+// Scans all of the folders in paths for media files. New files will be added to the
+// database.
+func (lib *LocalLibrary) Scan() {
+	// Make sure there are no other scans working at the moment
+	lib.WaitScan()
+
+	start := time.Now()
+	mediaChan := make(chan string, 100)
+
+	lib.scanWG.Add(1)
+	go lib.databaseWriter(mediaChan, &lib.scanWG)
+
+	lib.initializeWatcher()
+
+	for _, path := range lib.paths {
+		lib.walkWG.Add(1)
+		go lib.scanPath(path, mediaChan)
+	}
+
+	lib.scanWG.Add(1)
+	go func() {
+		defer func() {
+			log.Printf("Walking took %s", time.Since(start))
+			lib.scanWG.Done()
+		}()
+		lib.walkWG.Wait()
+		close(mediaChan)
+	}()
+
+	go func() {
+		lib.WaitScan()
+		log.Printf("Scaning took %s", time.Since(start))
+	}()
+}
+
+// Blocks the current goroutine until the scan has been finished
+func (lib *LocalLibrary) WaitScan() {
+	lib.scanWG.Wait()
+}
+
+// This is the goroutine which actually scans a library path.
+// For now it ignores everything but the list of supported files. It is so
+// because jplayer cannot play anything else. Sends every suitable
+// file into the media channel
+func (lib *LocalLibrary) scanPath(scannedPath string, media chan<- string) {
+	start := time.Now()
+
+	defer func() {
+		log.Printf("Walking %s took %s", scannedPath, time.Since(start))
+		lib.walkWG.Done()
+	}()
+
+	walkFunc := func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			log.Println(err)
+			return nil
+		}
+
+		if lib.isSupportedFormat(path) {
+			media <- path
+		}
+
+		if lib.watch != nil && info.IsDir() {
+			//log.Printf("Adding watch for %s\n", path)
+			lib.watch.Watch(path)
+		}
+
+		return nil
+	}
+
+	err := filepath.Walk(scannedPath, walkFunc)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
---
+++ src/library/local_library_watch.go
@@ -0,0 +1,132 @@
+package library
+
+import (
+	"log"
+	"os"
+
+	"github.com/howeyc/fsnotify"
+)
+
+// Creates the directory watcher if none was created before. On failure logs the
+// problem and leaves the watcher unintialized. LocalLibrary should work even
+// without a watch.
+func (lib *LocalLibrary) initializeWatcher() {
+	if lib.watch != nil {
+		return
+	}
+	newWatcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Printf("Directory watcher was not initialized properly. ")
+		log.Printf("New files will not be added to the library. Reason: ")
+		log.Println(err)
+		return
+	}
+	lib.watch = newWatcher
+	lib.watchMediaChan = make(chan string, 100)
+	lib.watchClosedChan = make(chan bool)
+
+	go lib.databaseWriter(lib.watchMediaChan, nil)
+
+	go lib.watchEventRoutine()
+}
+
+// Stops the filesystem watching and all supporing it goroutines.
+func (lib *LocalLibrary) stopWatcher() {
+	if lib.watch != nil {
+		lib.watchClosedChan <- true
+		lib.watch.Close()
+		lib.watch = nil
+		close(lib.watchMediaChan)
+		close(lib.watchClosedChan)
+	}
+}
+
+// This function is resposible for selecting the watcher events
+func (lib *LocalLibrary) watchEventRoutine() {
+
+	// To make sure we will not write in the database at the same time as the
+	// scanning goroutines we will wait them to end.
+	lib.WaitScan()
+	defer func() {
+		log.Println("Directory watcher event receiver stopped.")
+	}()
+
+	if lib.watch == nil {
+		return
+	}
+
+	for {
+		select {
+		case ev := <-lib.watch.Event:
+			if ev == nil {
+				return
+			}
+			lib.handleWatchEvent(ev)
+		case err := <-lib.watch.Error:
+			if err == nil {
+				return
+			}
+			log.Println("Directory watcher error:", err)
+		case <-lib.watchClosedChan:
+			return
+		}
+	}
+}
+
+// Deals with the watcher events.
+//  * new directories should be watched and they themselves scanned
+//  * new files should be added to the library
+//  * deleted files should be removed from the library
+//  * deleted directories should be unwatched
+//  * modfied files should be updated in the database
+//  * renamed ...
+func (lib *LocalLibrary) handleWatchEvent(event *fsnotify.FileEvent) {
+	// log.Println("Watch event:", event)
+
+	if event.IsAttrib() {
+		// The event was just an attribute change
+		return
+	}
+
+	st, stErr := os.Stat(event.Name)
+
+	if stErr != nil && !event.IsRename() && !event.IsDelete() {
+		log.Printf("Watch event stat received error: %s\n", stErr.Error())
+		return
+	}
+
+	if event.IsCreate() && st.IsDir() {
+		lib.watch.Watch(event.Name)
+		lib.walkWG.Add(1)
+		go lib.scanPath(event.Name, lib.watchMediaChan)
+		return
+	}
+
+	if event.IsCreate() && !st.IsDir() {
+		if lib.isSupportedFormat(event.Name) {
+			lib.watchMediaChan <- event.Name
+		}
+		return
+	}
+
+	if event.IsModify() && !st.IsDir() {
+		if lib.isSupportedFormat(event.Name) {
+			lib.removeFile(event.Name)
+			lib.watchMediaChan <- event.Name
+		}
+		return
+	}
+
+	if event.IsDelete() || event.IsRename() {
+		if lib.isSupportedFormat(event.Name) {
+			// This is a file
+			lib.removeFile(event.Name)
+			return
+		} else {
+			// It was a directory... probably
+			lib.watch.RemoveWatch(event.Name)
+			lib.removeDirectory(event.Name)
+			return
+		}
+	}
+}
