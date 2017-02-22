91373908973f5d0044b28e3d9251d7a7713c5e34
Doychin Atanasov <ironsmile@gmail.com>
2017-02-22 21:41:49 +0200

    Remove the daemonizing binary
    No one is using it anyway. HTTPMS can be run with initV/upstart/systemd
    or whatever. An init script for systemd is supplied in the repo already.

--- src/daemon/daemon_nix.go
+++ src/daemon/daemon_nix.go
@@ -5,51 +5,11 @@
 // daemonizing. For Windows - I don't know yet.
 package daemon
 
-import (
-	"fmt"
-	"os"
-	"syscall"
-)
+import "syscall"
 
 // StopSignals contains all the signals which will make our daemon remove its pidfile
 var StopSignals = []syscall.Signal{
 	syscall.SIGINT,
 	syscall.SIGKILL,
 	syscall.SIGTERM,
-}
-
-// Daemonize is the main function for this module. It should be run only once.
-func Daemonize() error {
-	var ret uintptr
-	var err syscall.Errno
-
-	ret, _, err = syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
-	if err != 0 {
-		return err
-	}
-	switch ret {
-	case 0:
-		break
-	default:
-		os.Exit(0)
-	}
-
-	st, e := syscall.Setsid()
-	if st == -1 {
-		return fmt.Errorf("Setsid returned %d", st)
-	}
-	if e != nil {
-		return e
-	}
-	os.Chdir("/")
-
-	f, e := os.OpenFile(os.DevNull, os.O_RDWR, 0)
-	if e == nil {
-		fd := f.Fd()
-		syscall.Dup2(int(fd), int(os.Stdin.Fd()))
-		syscall.Dup2(int(fd), int(os.Stdout.Fd()))
-		syscall.Dup2(int(fd), int(os.Stderr.Fd()))
-	}
-
-	return nil
 }
