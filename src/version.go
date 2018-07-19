6c69ec2cc95e54f1466900ee2f5d0c9605c258e4
Doychin Atanasov <ironsmile@gmail.com>
2018-07-20 01:10:13 +0300

    Document the `Version` variable in the `src` pacakge

--- src/version.go
+++ src/version.go
@@ -5,7 +5,8 @@ import (
 	"runtime"
 )
 
-var Version = "v1.2.0-development"
+// Version stores the current verion of HTTPMS. It is set during building.
+var Version = "v1.2-source"
 
 func printVersionInformation() {
 	fmt.Printf("HTTP Media Server (httpms) %s\n", Version)
