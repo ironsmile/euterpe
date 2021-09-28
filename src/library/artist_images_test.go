18fb6d1ac206788f687b8e216fb0e36e8afa7717
Doychin Atanasov <ironsmile@gmail.com>
2021-09-28 23:04:18 +0300

    Make sure artist image "not found" is cached for a while

--- src/library/artist_images_test.go
+++ src/library/artist_images_test.go
@@ -93,13 +93,18 @@ func TestLocalLibraryFindAndSaveArtistImage(t *testing.T) {
 		t.Fatalf("inserting media file failed: %s", err)
 	}
 
+	firstArtistID, err := lib.GetArtistID(mediaFile.artist)
+	if err != nil {
+		t.Errorf("error getting first artist ID: %s", err)
+	}
+
 	// Set-up finished. Actual tests start here. First try to find an image for
 	// an artist who does not have one in the database.
-	assertArtistImage(t, lib, 1, SmallImage, smallImage)
+	assertArtistImage(t, lib, firstArtistID, SmallImage, smallImage)
 
 	// Now search for the original image. It should have been stored in the database
 	// as part of creating the small one.
-	assertArtistImage(t, lib, 1, OriginalImage, bigImage)
+	assertArtistImage(t, lib, firstArtistID, OriginalImage, bigImage)
 
 	// Search for an image for artist who is not in the database at all.
 	_, err = lib.FindAndSaveArtistImage(ctx, 42, OriginalImage)
@@ -121,26 +126,68 @@ func TestLocalLibraryFindAndSaveArtistImage(t *testing.T) {
 		t.Fatalf("inserting second media file failed: %s", err)
 	}
 
-	err = lib.SaveArtistImage(ctx, 2, bytes.NewReader(secondBigImage))
+	secondArtistID, err := lib.GetArtistID(secondFile.artist)
+	if err != nil {
+		t.Errorf("error getting second artist ID: %s", err)
+	}
+
+	err = lib.SaveArtistImage(ctx, secondArtistID, bytes.NewReader(secondBigImage))
 	if err != nil {
 		t.Fatalf("error saving an artist image: %s", err)
 	}
-	assertArtistImage(t, lib, 2, OriginalImage, secondBigImage)
+	assertArtistImage(t, lib, secondArtistID, OriginalImage, secondBigImage)
 
 	// Now get the small version of this original image. This tests converting
 	// a big original in the database into the desired size when this size was
 	// not found.
-	assertArtistImage(t, lib, 2, SmallImage, smallImage)
+	assertArtistImage(t, lib, secondArtistID, SmallImage, smallImage)
 
-	// And finally, remove this artist's image from the database and make sure it is
+	// Remove this artist's image from the database and make sure it is
 	// deleted.
-	if err = lib.RemoveArtistImage(ctx, 2); err != nil {
+	if err = lib.RemoveArtistImage(ctx, secondArtistID); err != nil {
 		t.Fatalf("error removing artist image: %s", err)
 	}
 
-	_, err = lib.FindAndSaveArtistImage(ctx, 2, OriginalImage)
+	_, err = lib.FindAndSaveArtistImage(ctx, secondArtistID, OriginalImage)
 	if !errors.Is(err, ErrArtworkNotFound) {
 		t.Fatalf("expected artwork not found error but got `%+v`", err)
+	}
+
+	// Insert a new artist and make sure it caches the "not-found" response at least
+	// for a while.
+	alwaysNotFoundFinder := &artfakes.FakeFinder{
+		GetArtistImageStub: func(_ context.Context, _ string) ([]byte, error) {
+			return nil, art.ErrImageNotFound
+		},
+	}
+	lib.SetArtFinder(alwaysNotFoundFinder)
+
+	thirdFile := MockMedia{
+		artist: "Not Foundoff",
+		album:  "The Test Strikes Back",
+		title:  "Always Not There For You",
+		track:  3,
+		length: 223,
+	}
+	thirdFilePath := filepath.FromSlash("path/to/not-foundouff.mp3")
+	if err := lib.insertMediaIntoDatabase(&thirdFile, thirdFilePath); err != nil {
+		t.Fatalf("inserting third media file failed: %s", err)
+	}
+
+	thirdArtistID, err := lib.GetArtistID(thirdFile.artist)
+	if err != nil {
+		t.Errorf("error getting second artist ID: %s", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		_, err = lib.FindAndSaveArtistImage(ctx, thirdArtistID, OriginalImage)
+		if !errors.Is(err, ErrArtworkNotFound) {
+			t.Fatalf("expected artwork not found error but got `%+v`", err)
+		}
+	}
+
+	if alwaysNotFoundFinder.GetArtistImageCallCount() != 1 {
+		t.Error("expected artFinder.GetArtistImage to have been called only once")
 	}
 }
 
