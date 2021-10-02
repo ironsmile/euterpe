a550298a3663d5cdee109672c508247b85fd2380
Doychin Atanasov <ironsmile@gmail.com>
2021-10-02 23:23:59 +0300

    Add tests for the rest of the fucs in config package

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -3,10 +3,12 @@ package config_test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/ironsmile/euterpe/src/config"
+	"github.com/spf13/afero"
 )
 
 // TestScanSectionUnmarshalJSON makes sure that decoding the JSON for the "library_scan"
@@ -37,5 +39,85 @@ func TestScanSectionUnmarshalJSON(t *testing.T) {
 
 	if ss != expected {
 		t.Errorf("expected `%+v` but got `%+v`", expected, ss)
+	}
+}
+
+// TestFindAndParseCreatesConfig makes sure that a new configuration file is created
+// when there was not when run.
+func TestFindAndParseCreatesConfig(t *testing.T) {
+	testfs := afero.NewMemMapFs()
+
+	_, err := config.FindAndParse(testfs)
+	if err != nil {
+		t.Fatalf("error finding and parsing configuration file: %s", err)
+	}
+
+	configPath := config.UserConfigPath(testfs)
+	st, err := testfs.Stat(configPath)
+	if err != nil {
+		t.Fatalf("error on stat for config file `%s`: %s", configPath, err)
+	}
+
+	if !st.Mode().IsRegular() {
+		t.Errorf("expected the configuration file to be regular file")
+	}
+}
+
+// TestFindAndParse makes sure that a file which is already created is read
+// and parsed.
+func TestFindAndParse(t *testing.T) {
+	testfs := afero.NewMemMapFs()
+
+	configPath := config.UserConfigPath(testfs)
+
+	const (
+		listenAddress = "1.2.3.4:1234"
+		user          = "test-user"
+		pass          = "test-pass"
+		secret        = "test-secret"
+	)
+
+	// Function is used for easier clean-up with defer.
+	func() {
+		fh, err := testfs.Create(configPath)
+		if err != nil {
+			t.Fatalf("error setting up test, config file create: %s", err)
+		}
+		defer fh.Close()
+
+		fmt.Fprintf(fh, `{
+			"listen": "%s",
+			"basic_authenticate": true,
+			"authentication": {
+				"user": "%s",
+				"password": "%s",
+				"secret": "%s"
+			}
+		}`, listenAddress, user, pass, secret)
+	}()
+
+	cfg, err := config.FindAndParse(testfs)
+	if err != nil {
+		t.Fatalf("error finding and parsing configuration file: %s", err)
+	}
+
+	if cfg.Listen != listenAddress {
+		t.Errorf("expected listen address `%s` but got `%s`", listenAddress, cfg.Listen)
+	}
+
+	if !cfg.Auth {
+		t.Error("expected basic authenticate to be True but it was not")
+	}
+
+	if cfg.Authenticate.User != user {
+		t.Errorf("expected username `%s` but got `%s`", cfg.Authenticate.User, user)
+	}
+
+	if cfg.Authenticate.Password != pass {
+		t.Errorf("expected password `%s` but got `%s`", cfg.Authenticate.Password, pass)
+	}
+
+	if cfg.Authenticate.Secret != secret {
+		t.Errorf("expected secret `%s` but got `%s`", cfg.Authenticate.Secret, secret)
 	}
 }
