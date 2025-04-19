0ec4faaca6adf5845f8432ba7ecb7be600aa7386
Doychin Atanasov <ironsmile@gmail.com>
2025-04-19 20:28:28 +0300

    Add tests for wrong input during radio management

--- src/radio/radio_test.go
+++ src/radio/radio_test.go
@@ -2,9 +2,11 @@ package radio_test
 
 import (
 	"context"
+	"errors"
 	"io/fs"
 	"net/url"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/ironsmile/euterpe/src/library"
@@ -88,6 +90,102 @@ func TestRadioManager(t *testing.T) {
 	}
 	if len(allRadios) != 0 {
 		t.Fatalf("Expected no radios to be returned but there were %d", len(allRadios))
+	}
+}
+
+// TestRadioManagerErrors checks that various errors are returned on invalid input.
+func TestRadioManagerErrors(t *testing.T) {
+	ctx := context.Background()
+
+	lib := getLibrary(ctx, t)
+	defer func() {
+		_ = lib.Truncate()
+	}()
+	radios := radio.NewManager(lib.ExecuteDBJobAndWait)
+
+	playURL, _ := url.Parse("http://test-radio.example.com/play.mp3")
+	homepageURL, _ := url.Parse("http://test-radio.example.com")
+
+	goodStation := radio.Station{
+		Name:      "Test Radio",
+		StreamURL: *playURL,
+		HomePage:  homepageURL,
+	}
+
+	noName := goodStation
+	noName.Name = ""
+
+	if _, err := radios.Create(ctx, noName); err == nil {
+		t.Errorf("Expected error for radio without name but got none")
+	} else if !strings.Contains(err.Error(), "name") {
+		t.Logf("Error returned: %s\n", err)
+		t.Errorf("Expected the error to mention that no name was set")
+	}
+
+	ftpURL, _ := url.Parse("ftp://test-radio.example.com")
+
+	badPlaySchema := goodStation
+	badPlaySchema.StreamURL = *ftpURL
+	if _, err := radios.Create(ctx, badPlaySchema); err == nil {
+		t.Errorf("Expected error for radio FTP play URL but got none")
+	} else if !strings.Contains(err.Error(), "scheme") {
+		t.Logf("Error returned for FTP stream URL: %s\n", err)
+		t.Errorf("Expected the error to mention that URL scheme is not supported")
+	}
+
+	badHomeSchema := goodStation
+	badHomeSchema.HomePage = ftpURL
+	if _, err := radios.Create(ctx, badHomeSchema); err == nil {
+		t.Errorf("Expected error for radio FTP home URL but got none")
+	} else if !strings.Contains(err.Error(), "scheme") {
+		t.Logf("Error returned for FTP home: %s\n", err)
+		t.Errorf("Expected the error to mention that URL scheme is not supported")
+	}
+
+	if err := radios.Delete(ctx, 5823); !errors.Is(err, radio.ErrNotFound) {
+		t.Errorf("Expected 'not found' error but got %s", err)
+	}
+
+	stationID, err := radios.Create(ctx, goodStation)
+	if err != nil {
+		t.Fatalf("Cannot create a station for testing: %s", err)
+	}
+
+	goodStation.ID = stationID
+
+	replaced := goodStation
+	replaced.Name = "Replaced Station"
+	replaced.ID = goodStation.ID + 1
+
+	if err := radios.Replace(ctx, replaced); !errors.Is(err, radio.ErrNotFound) {
+		t.Errorf("Expected 'not found' error but got %s", err)
+	}
+
+	replaced = goodStation
+	replaced.Name = ""
+	if err := radios.Replace(ctx, replaced); err == nil {
+		t.Errorf("Expected error for replaced radio without name but got none")
+	} else if !strings.Contains(err.Error(), "name") {
+		t.Logf("Error returned: %s\n", err)
+		t.Errorf("Expected the error for replaced radio to mention that no name was set")
+	}
+
+	replaced = goodStation
+	replaced.StreamURL = *ftpURL
+	if err := radios.Replace(ctx, replaced); err == nil {
+		t.Errorf("Expected error for replaced radio FTP play URL but got none")
+	} else if !strings.Contains(err.Error(), "scheme") {
+		t.Logf("Error returned for FTP stream URL: %s\n", err)
+		t.Errorf("Expected the error to mention that URL scheme is not supported")
+	}
+
+	replaced = goodStation
+	replaced.HomePage = ftpURL
+	if err := radios.Replace(ctx, replaced); err == nil {
+		t.Errorf("Expected error for replaced radio home page URL but got none")
+	} else if !strings.Contains(err.Error(), "scheme") {
+		t.Logf("Error returned for home page URL: %s\n", err)
+		t.Errorf("Expected the error to mention that URL scheme is not supported")
 	}
 }
 
